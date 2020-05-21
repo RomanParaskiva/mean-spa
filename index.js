bf4eda097e9baf7623970365121137fc84cc1a76
Roman Paraskiva <parasta2018@gmail.com>
2020-05-21 18:00:31 +0300

    make it better

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const request = require('request');
-const settings = require('./routes/settings.route');
 
 const app = express();
 
